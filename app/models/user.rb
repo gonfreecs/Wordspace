c1a8eee2228aef595baebe2ccd9b39fb73b7c883
mayarali <mayar.bassel.ali@gmail.com>
2015-03-12 15:31:39 +0200

    #1 adding extra fields to users + sponsor sign up frontend and backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,13 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
  	#:recoverable, 
 
+ #making firstname and lastname required
+ validates :firstname, :presence => true
+ validates :lastname, :presence => true
+ 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+ 	#:recoverable,
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 end
