dfd8abd6f2e2ec7aea17cccc931be58dee95eca0
Andrew Magdy <andrewtawf@gmail.com>
2015-04-01 01:07:54 +0200

    #44 Changed Default Landing Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  root 'articles#index'
   resources :sponsors
   resources :articles
   resources :comments
   resources :replies
-
-  get 'main/index'
   get 'tags/:tag', to: 'articles#index', as: :tag
 
   mount RedactorRails::Engine => '/redactor_rails'
