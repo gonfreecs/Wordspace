b81dfbc20f0a5fed621ac26b63e417be8da41fa5
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2015-04-05 13:11:55 +0200

    #45 Added relation to users with magazines

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ActiveRecord::Base
   validates :lastname, presence: true
   # adding relation between user and articles
   has_many :articles
+  # Author: Mohammed El-Ansary
+  # 5.4.2015
+  # Adding relation between user and magazines
+  has_and_belongs_to_many :magazines
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   #:recoverable,
