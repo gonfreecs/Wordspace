ecfb336014e5a2bb9b6440d37165e1e296dc3008
Mina Hany <monzbasha@yahoo.com>
2015-04-05 12:34:11 +0200

    Revert "#39"
    This reverts commit a87304359b5bd632ea1ec65a884ddc884d4eacf2.

--- spec/models/reportarticle.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe Contact do
-  it "has a valid factory"
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "returns a contact's full name as a string"
-end
