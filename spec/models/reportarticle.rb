a87304359b5bd632ea1ec65a884ddc884d4eacf2
Mina Hany <monzbasha@yahoo.com>
2015-04-05 12:33:43 +0200

    #39

---
+++ spec/models/reportarticle.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Contact do
+  it "has a valid factory"
+  it "is invalid without a firstname"
+  it "is invalid without a lastname"
+  it "returns a contact's full name as a string"
+end
