7f01b640e3972ca1144806c0bd472fa0d70704cb
MohammedAnsary <a.n.s.a.r.y@hotmail.com>
2015-04-07 14:48:28 +0200

    #35 Tests Added

---
+++ spec/controllers/search_controller_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+RSpec.describe SearchController, type: :controller do
+  describe 'GET #search' do
+    before(:each) do
+      request.env['HTTP_REFERER'] = 'test'
+    end
+    it 'returns correct search results' do
+      a1 = create(:article)
+      a2 = create(:article, title: 'Test', plain_body: 'xxxxtestxxxx')
+      get :search, query: 'xxxxtestxxxx'
+      expect(assigns(:articles)).to match_array([a1, a2])
+    end
+    it 'redirects to same page if empty query' do
+      request.env['HTTP_REFERER']
+      get :search, query: ''
+      response.should redirect_to 'test'
+    end
+    it 'does not allow extra params' do
+      get :search, query: 'test', name: 'test'
+      puts :query
+      puts :name
+      assigns(:name).should.nil?
+      assigns(:query).should == 'test'
+    end
+  end
+end
