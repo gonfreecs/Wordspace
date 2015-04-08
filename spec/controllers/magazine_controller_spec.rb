47c2555de40171d542853bd8dc0f8e9d26b84078
MinaHany94 <monzbasha@yahoo.com>
2015-04-08 22:09:46 +0200

    #61 tests modified

--- spec/controllers/magazine_controller_spec.rb
+++ spec/controllers/magazine_controller_spec.rb
@@ -9,9 +9,15 @@ RSpec.describe MagazinesController, type: :controller do
       expect(response).to render_template('showrequests')
     end
 
+    it 'responds successfully with an HTTP 200 status code' do
+      get :showrequests
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
     it 'shows joining requests correctly' do
-      a1 = create(:article)
-      a2 = create(:article, title: 'Test', plain_body: 'xxxxtestxxxx')
+      r1 = create(:requestjoiningmagazine)
+      r2 = create(:article, title: 'Test', plain_body: 'xxxxtestxxxx')
       get :search, query: 'xxxxtestxxxx'
       expect(assigns(:articles)).to match_array([a1, a2])
     end
