4c017fadc9fb6dbcd6bec61b68aa8d3c9fe5da14
MinaHany94 <monzbasha@yahoo.com>
2015-04-08 23:38:39 +0200

    #43 documentation modified @bassemmagdy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  
+
     #loading all variables
 	load_and_authorize_resource
 	#rescue from invalid articles ids
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 	    raise(CanCan::AccessDenied, 'Comment is not found')
 		  }
   def create
-  
+
   # @comment = Comment.new(params[:comment])
     if @comment.save
       flash[:notice] = 'Comment was successfully created.'
@@ -16,10 +16,10 @@ class CommentsController < ApplicationController
       flash[:notice] = "Error creating comment: #{@comment.errors}"
       redirect_to(@comment.article)
     end
-    
+
   end
- 
-  
+
+
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
@@ -42,17 +42,21 @@ class CommentsController < ApplicationController
       flash[:notice] = "Error creating comment: #{@comment.errors}"
       redirect_to(@comment.article)
     end
-    
+
   end
   def edit
   end
   def new
   end
-  #Report a comment
-  def report  
+
+  def report
+  #Author:Mina Hany
+  #8.4.2015
+  #A hash is created containing user's id who wants to report a comment
+  #and that comment's id and it is added to moedl contaioning reported requests
   reportcommenth = {"user_id" => current_user.id,
                     "comment_id" => @comment.id}
-  Reportcomment.create(reportcommenth)   
+  Reportcomment.create(reportcommenth)
    @article =Article.find( params[:id])
   redirect_to @article
   end
