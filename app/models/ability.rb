62ee102fadbf247f576a98772fb8728c8664c901
Mohab <mohabashraf466@gmail.com>
2015-04-08 05:52:08 +0200

    #58 add to html promotion button

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,48 +1,48 @@
-# this model is for CanCan abilities. Guests can only view the article. If the logged in user is the writer of the article he/she is viewing, he/she can edit it and if he/she is a moderator, he/she can delete the artcle
-# You don't need to be signed in to view Comments or replies. But, you need to be signed in in order to add any of them. Also you can delete or edit them if you wrote them. A moderator can delete any of them.
-class Ability
-  include CanCan::Ability
-  def initialize(user)
-    can :show, Article
-    can :show, Comment
-    can :show, Reply
-    unless user.nil?
-      can :update, Article do |article|
-        article.user_id == user.id
-      end
-      can :create, Article
+  # this model is for CanCan abilities. Guests can only view the article. If the logged in user is the writer of the article he/she is viewing, he/she can edit it and if he/she is a moderator, he/she can delete the artcle
+  # You don't need to be signed in to view Comments or replies. But, you need to be signed in in order to add any of them. Also you can delete or edit them if you wrote them. A moderator can delete any of them.
+  class Ability
+    include CanCan::Ability
+    def initialize(user)
+      can :show, Article
+      can :show, Comment
+      can :show, Reply
+      unless user.nil?
+        can :update, Article do |article|
+          article.user_id == user.id
+        end
+        can :create, Article
 
-      can :update, Comment do |c|
-        c.user_id == user.id
-      end
-      # mohab
-      # 5.4.2015
-      # check  promote  if current user 
-      # and not promoted before
-      # and user budget greater than 4000
-         can :promotion, Article do |article|
-        article.user_id == user.id
-        article.promoted == false
-        user.budget > 1000 
-      end
-      can :destroy, Comment do |co|
-        co.user_id == user.id
-      end
-      can :create, Comment
+        can :update, Comment do |c|
+          c.user_id == user.id
+        end
+        # mohab
+        # 5.4.2015
+        # check  promote  if current user 
+        # and not promoted before
+        # and user budget greater than 4000
+           can :promotion, Article do |article|
+          article.user_id == user.id
+          article.promoted == false
+          user.budget > 1000 
+        end
+        can :destroy, Comment do |co|
+          co.user_id == user.id
+        end
+        can :create, Comment
 
-      can :update, Reply do |r|
-        r.user_id == user.id
-      end
-      can :destroy, Reply do |ro|
-        ro.user_id == user.id
-      end
-      can :create, Reply
+        can :update, Reply do |r|
+          r.user_id == user.id
+        end
+        can :destroy, Reply do |ro|
+          ro.user_id == user.id
+        end
+        can :create, Reply
 
-      if user.is_moderator
-        can :destroy, Article
-        can :destroy, Comment
-        can :destroy, Reply
+        if user.is_moderator
+          can :destroy, Article
+          can :destroy, Comment
+          can :destroy, Reply
+        end
       end
     end
-  end
- end
+   end
