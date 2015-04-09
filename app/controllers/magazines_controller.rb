33b3e8e43a55ede59ff18569a80bba26f6f95a4c
mayarali <mayar.bassel.ali@gmail.com>
2015-04-09 18:40:08 +0200

    #33 indentation fixed in controllers

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -1,91 +1,91 @@
-# Author:Mohammed El-Ansary
-# 5.4.2015
-# Generated through scaffolding Magazine
-class MagazinesController < ApplicationController
-  # loading all variables
-  load_and_authorize_resource
-  # rescue from invalid articles ids
-  rescue_from(ActiveRecord::RecordNotFound) do
-    fail(CanCan::AccessDenied, 'Article is not found')
-  end
-  before_action :set_magazine, only: [:show, :edit, :update, :destroy]
-  # check if cancel => no updates performed
-  before_action :check_for_cancel, only: [:create, :update]
+  # Author:Mohammed El-Ansary
+  # 5.4.2015
+  # Generated through scaffolding Magazine
+  class MagazinesController < ApplicationController
+    # loading all variables
+    load_and_authorize_resource
+    # rescue from invalid articles ids
+    rescue_from(ActiveRecord::RecordNotFound) do
+      fail(CanCan::AccessDenied, 'Article is not found')
+    end
+    before_action :set_magazine, only: [:show, :edit, :update, :destroy]
+    # check if cancel => no updates performed
+    before_action :check_for_cancel, only: [:create, :update]
 
-  # GET /magazines
-  # GET /magazines.json
-  def index
-    @magazines = Magazine.all
-  end
+    # GET /magazines
+    # GET /magazines.json
+    def index
+      @magazines = Magazine.all
+    end
 
-  # GET /magazines/1
-  # GET /magazines/1.json
-  def show
-  end
+    # GET /magazines/1
+    # GET /magazines/1.json
+    def show
+    end
 
-  # GET /magazines/new
-  def new
-    @magazine = Magazine.new
-  end
+    # GET /magazines/new
+    def new
+      @magazine = Magazine.new
+    end
 
-  # GET /magazines/1/edit
-  def edit
-  end
+    # GET /magazines/1/edit
+    def edit
+    end
 
-  # POST /magazines
-  # POST /magazines.json
-  def create
-    @magazine.users << current_user
-    respond_to do |format|
-      if @magazine.save
-        format.html { redirect_to @magazine, notice: 'Magazine was successfully created.' }
-        format.json { render :show, status: :created, location: @magazine }
-      else
-        format.html { render :new }
-        format.json { render json: @magazine.errors, status: :unprocessable_entity }
+    # POST /magazines
+    # POST /magazines.json
+    def create
+      @magazine.users << current_user
+      respond_to do |format|
+        if @magazine.save
+          format.html { redirect_to @magazine, notice: 'Magazine was successfully created.' }
+          format.json { render :show, status: :created, location: @magazine }
+        else
+          format.html { render :new }
+          format.json { render json: @magazine.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /magazines/1
-  # PATCH/PUT /magazines/1.json
-  def update
-    respond_to do |format|
-      if @magazine.update(magazine_params)
-        format.html { redirect_to @magazine, notice: 'Magazine was successfully updated.' }
-        format.json { render :show, status: :ok, location: @magazine }
-      else
-        format.html { render :edit }
-        format.json { render json: @magazine.errors, status: :unprocessable_entity }
+    # PATCH/PUT /magazines/1
+    # PATCH/PUT /magazines/1.json
+    def update
+      respond_to do |format|
+        if @magazine.update(magazine_params)
+          format.html { redirect_to @magazine, notice: 'Magazine was successfully updated.' }
+          format.json { render :show, status: :ok, location: @magazine }
+        else
+          format.html { render :edit }
+          format.json { render json: @magazine.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /magazines/1
-  # DELETE /magazines/1.json
-  def destroy
-    @magazine.destroy
-    respond_to do |format|
-      format.html { redirect_to magazines_url, notice: 'Magazine was successfully destroyed.' }
-      format.json { head :no_content }
+    # DELETE /magazines/1
+    # DELETE /magazines/1.json
+    def destroy
+      @magazine.destroy
+      respond_to do |format|
+        format.html { redirect_to magazines_url, notice: 'Magazine was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+
+    # Cancel an update and return to magazine page
+    def check_for_cancel
+      return false if params[:commit] != 'Cancel'
+      redirect_to @magazine
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_magazine
+      @magazine = Magazine.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def magazine_params
+      params.require(:magazine).permit(:name, :decription, :image)
     end
   end
-
-  # Cancel an update and return to magazine page
-  def check_for_cancel
-    return false if params[:commit] != 'Cancel'
-    redirect_to @magazine
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_magazine
-    @magazine = Magazine.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def magazine_params
-    params.require(:magazine).permit(:name, :decription, :image)
-  end
-end
