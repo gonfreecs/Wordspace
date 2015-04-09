4aeb79dae24fb6a6455842b2359b907c4476128f
mayarali <mayar.bassel.ali@gmail.com>
2015-04-09 23:42:08 +0200

    #34 fixing code accept/reject

--- app/controllers/collaboration_invitations_controller.rb
+++ app/controllers/collaboration_invitations_controller.rb
@@ -1,33 +1,32 @@
-# Author: Mayar
-# Date: 8.4.2015
-# Invitation Controller
-class CollaborationInvitationsController < ApplicationController
-
   # Author: Mayar
   # Date: 8.4.2015
-  # Showing invitations to that user
-  def index
-    @reqinv = CollaborationInvitation.where(:User_id => current_user.id)
-  end
+  # Invitation Controller
+  class CollaborationInvitationsController < ApplicationController
+    # Author: Mayar
+    # Date: 8.4.2015
+    # Showing invitations to that user
+    def index
+      @reqinv = CollaborationInvitation.where(User_id: current_user.id)
+    end
 
-  # Author: Mayar
-  # Date: 8.4.2015
-  # Approving invitation
-  def check_for_approve
-    @invite = CollabotaionInvitation.find(params[:id])
-    @magazne = Magazine.find(@invite.Magazine_id)
-    @magazine.users << current_user.id
-    Collabotation.destroy.where(:User_id => current_user.id, :Magazine_id => @magazine.id)
-    redirect_to invitations_path
-  end
+    # Author: Mayar
+    # Date: 8.4.2015
+    # Approving invitation
+    def check_for_approve
+      @invite = CollaborationInvitation.find(params[:id])
+      @magazine = Magazine.find(@invite.Magazine_id)
+      @magazine.users << current_user
+      @invite.destroy
+      @invite.save
+      redirect_to collaboration_invitations_path
+    end
 
-  # Author: Mayar
-  # Date: 7.4.2015
-  # Rejecting invitation
-  def check_for_reject
-
-    @record = CollaborationInvitation.find(params[:i2])
-    ColloborationInvitation.destroy(@record)
-    redirect_to invitations_path
+    # Author: Mayar
+    # Date: 7.4.2015
+    # Rejecting invitation
+    def check_for_reject
+      @record = CollaborationInvitation.find(params[:id])
+      @record.destroy
+      redirect_to collaboration_invitations_path
+    end
   end
-end
