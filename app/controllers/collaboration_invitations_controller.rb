36d511c50016cd13fd8443337860970834a5e022
mayarali <mayar.bassel.ali@gmail.com>
2015-04-09 14:13:50 +0200

    #34 fixing checks

--- app/controllers/collaboration_invitations_controller.rb
+++ app/controllers/collaboration_invitations_controller.rb
@@ -2,33 +2,30 @@
 # Date: 8.4.2015
 # Invitation Controller
 class CollaborationInvitationsController < ApplicationController
-  
+
   # Author: Mayar
   # Date: 8.4.2015
   # Showing invitations to that user
   def index
-    @reqinv = CollaborationInvitation.where(User_id: :current_user_id)
+    @reqinv = CollaborationInvitation.where(:User_id => current_user.id)
   end
 
   # Author: Mayar
   # Date: 8.4.2015
   # Approving invitation
   def check_for_approve
-    
-    @i = params[:i2]
-    @user = User.find(@i.User_id)
-    @magazne = Magazine.find(@i.Magazine_id)
-    @magazine.users << @user
-    @record = CollaborationInvitation.find(@i)
-    ColloborationInvitation.destroy(@record)
+    @invite = CollabotaionInvitation.find(params[:id])
+    @magazne = Magazine.find(@invite.Magazine_id)
+    @magazine.users << current_user.id
+    Collabotation.destroy.where(:User_id => current_user.id, :Magazine_id => @magazine.id)
     redirect_to invitations_path
   end
-  
+
   # Author: Mayar
   # Date: 7.4.2015
   # Rejecting invitation
   def check_for_reject
-    
+
     @record = CollaborationInvitation.find(params[:i2])
     ColloborationInvitation.destroy(@record)
     redirect_to invitations_path
