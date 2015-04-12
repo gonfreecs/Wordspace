c598a19bb352b3b6ad7eaed1687321fec61d5738
omar Essam <eng.omar.essamel.din@gmail.com>
2015-04-12 12:47:51 +0200

    #62 adding documentation

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,7 +1,7 @@
 # Author: Omar Essam
 # created at: 9/4/2015
 # model for ads every ad belongs to a user
-# arguments are: title, user_id,& description
+# arguments are: title, user_id,&  description
 class Ad < ActiveRecord::Base
   belongs_to :user
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,7 +1,7 @@
     # Author: Omar Essam
     # created at: 1/4/2015
     # model for bids every bid belongs to a user and an article
-    # arguments are user_id, article_id, offer, & ad_id
+    # arguments are user_id, article_id, offer, & ad_id, & status
     class Bid < ActiveRecord::Base
       enum status: [:accepted, :rejected, :pending]
 
