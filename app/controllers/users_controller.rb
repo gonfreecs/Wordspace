a0fd2680de6a2a4c406015f99869d077151386cc
Mohab <mohabashraf466@gmail.com>
2015-04-15 17:20:37 +0200

    #31 solve conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
-	# mohab
-	# 1.4.2015
-	# user
-	# show profile page
-	class UsersController < ApplicationController
-	  before_filter :authenticate_user!
-	  def show
-	    @user = User.find(params[:id])
-	    @articles = @user.articles
-	  end
-	end
+  # mohab
+  # 1.4.2015
+  # user
+  # show profile page
+  class UsersController < ApplicationController
+    before_filter :authenticate_user!
+    def show
+      @user = User.find(params[:id])
+      @articles = @user.articles
+    end
+  end
