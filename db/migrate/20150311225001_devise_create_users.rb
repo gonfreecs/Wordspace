375d47af88d190415f69d3391d434e5fbdb2749e
Mohab <mohabashraf466@gmail.com>
2015-04-29 05:07:18 +0200

    solve

--- db/migrate/20150311225001_devise_create_users.rb
+++ db/migrate/20150311225001_devise_create_users.rb
@@ -38,4 +38,5 @@
       add_index :users, :reset_password_token, unique: true
       # add_index :users, :confirmation_token,   unique: true
       # add_index :users, :unlock_token,         unique: true
+    end
   end
