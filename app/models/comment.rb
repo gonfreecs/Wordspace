9778c86c5e1a6806624b00c4336b7f49be70ec4c
AndrewMagdy <andrewtawf@gmail.com>
2015-04-29 03:07:58 +0300

    Resolving Conflicts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,7 @@
 class Comment < ActiveRecord::Base
-<<<<<<< HEAD
   # A comment belongs to an Article
   # and has many replies
   # and has a field for description
-=======
-  # A comment belongs to an Article and has many replies and has a field for description
->>>>>>> master
   belongs_to :article
   validates_presence_of :des
   validates_length_of :des, within: 1..500
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-# User Model
-class User < ActiveRecord::Base
-  # to upload avatar for user
-  mount_uploader :avatar, AvatarUploader
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  # :recoverable,
-
-  # making firstname and lastname required
-  validates :firstname, presence: true
-  validates :lastname, presence: true
-  # adding relation between user and articles
-  has_many :articles
-  # Author: Mohammed El-Ansary
-  # 5.4.2015
-  # Adding relation between user and magazines
-  has_and_belongs_to_many :magazines
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  #:recoverable,
-  devise :database_authenticatable, :registerable,
-         :rememberable, :trackable, :validatable
-
-  # to upload avatar for user
-  validates_integrity_of :avatar
-  validates_processing_of :avatar
-end
-=======
   # User Model
   class User < ActiveRecord::Base
     # to upload avatar for user
@@ -59,4 +29,3 @@ end
     validates_integrity_of :avatar
     validates_processing_of :avatar
   end
->>>>>>> master
