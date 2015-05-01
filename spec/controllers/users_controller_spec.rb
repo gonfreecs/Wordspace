0293f1871a06cf5cba3a954a9547be1aecb9607f
omarelhariry <omarelhariry139@gmail.com>
2015-05-01 18:29:59 +0300

    #91 adding tests

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,46 @@
+# Author: Hariry
+# 29.4.2015
+# Test for user controls view
+require 'rails_helper'
+RSpec.describe UsersController, type: :controller do
+  # Author: Hariry
+  # 29.4.2015
+  # Test for user to view all his articles
+  describe 'GET #controls' do
+    it 'allows logged user to view all his articles' do
+      @user = create(:user)
+      sign_in @user
+      @article = create(:article, user_id: @user.id)
+      get :controls, user: FactoryGirl.attributes_for(:user)
+      expect(assigns(:my_articles)).to match_array([@article])
+    end
+  end
+  # Author: Hariry
+  # 29.4.2015
+  # Test for user to view promoted articles
+  describe 'GET #controls' do
+    it 'allows logged user to view his promoted articles' do
+      @user = create(:user)
+      sign_in @user
+      @article = create(:article, user_id: @user.id, promotevalue: 1000000)
+      @article2 = create(:article, user_id: @user.id, promotevalue: 2000000)
+      @article3 = create(:article, user_id: @user.id, promotevalue: 3000000)
+      get :controls, user: FactoryGirl.attributes_for(:user)
+      expect(assigns(:promoted_articles_1000)).to match_array([@article])
+      expect(assigns(:promoted_articles_2000)).to match_array([@article2])
+      expect(assigns(:promoted_articles_3000)).to match_array([@article3])
+    end
+  end
+  # Author: Hariry
+  # 29.4.2015
+  # Test for user to view sponsered articles
+  describe 'GET #controls' do
+    it 'allows logged user to view his sponsered articles' do
+      @user = create(:user)
+      sign_in @user
+      @article = create(:article, user_id: @user.id, is_sponsored: true)
+      get :controls, user: FactoryGirl.attributes_for(:user)
+      expect(assigns(:sponsered_articles)).to match_array([@article])
+    end
+  end
+end
